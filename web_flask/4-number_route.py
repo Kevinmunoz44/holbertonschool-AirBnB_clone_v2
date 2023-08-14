5990df92a539671b9fed5df9d0b1b520f075d986
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-14 15:26:24 -0500

    Update file 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''Use the module of flask'''
+from flask import Flask
+'''
+Initializate this application with flask
+'''
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text_format = text.replace('_', ' ')
+    result = f'C {text_format}'
+    return result
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    text_format = text.replace('_', ' ')
+    result = f'Python {text_format}'
+    return result
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
