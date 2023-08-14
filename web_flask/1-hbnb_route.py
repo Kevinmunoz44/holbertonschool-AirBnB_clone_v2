c1f29356aa801dc16025eadd683e02f48ca37742
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-14 14:30:12 -0500

    Update file 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Use the module of flask'''
+from flask import Flask
+'''
+Initializate this application with flask
+'''
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
