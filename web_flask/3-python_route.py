c877cfbaee48b7a8c716df659e16cb2087cbd23f
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-14 15:00:19 -0500

    Update file 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,6 @@ def c_is_fun(text):
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text):
-    text = 'is cool'
     text_format = text.replace('_', ' ')
     result = f'Python {text_format}'
     return result
