0219b66595a677bc23b86476acbfe8157158db1a
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-14 14:51:56 -0500

    Update file 2c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Use the module of flask'''
+from flask import Flask
+'''
+Initializate this application with flask
+'''
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text_format = text.replace('_', ' ')
+    result = f'C {text_format}'
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
