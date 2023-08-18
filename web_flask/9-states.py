11b439d2057b7cabc4d45675829f10bb78a62a55
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-08-18 11:33:39 -0500

    Update files 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
-'''Calling another modules and module of flask'''
+""" Starts a Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
-
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """tear down app context"""
+def dispose(exception):
+    """ Remove current session """
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def list_all_states():
-    """lists states from database
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('9-states.html', all_states=all_states)
+@app.route('/states/')
+@app.route('/states/<id>')
+def states_and_state(id=None):
+    """ Display list of all the states """
+    if id:
+        id = 'State.{}'.format(id)
+    return render_template('9-states.html', states=storage.all(State), id=id)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def find_state(id):
-    """lists states from database with specific id
-    Args:
-        id (str): id
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    all_states_id = []
-    for k, v in dict_states.items():
-        all_states_id.append(v.id)
-        all_states.append(v)
-    return render_template('9-states.html', all_states=all_states,
-                           all_states_id=all_states_id, id=id)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
